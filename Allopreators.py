efb0f9a9c2b4fc7d504b01bab7f5d9c1609945fc
sangramsir <136458988+sangramsir@users.noreply.github.com>
2023-06-15 22:30:56 +0530

    Add files via upload

---
+++ Allopreators.py
@@ -0,0 +1,96 @@
+# arithmetic operator
+a=10
+b=5
+print("Addition of ",a,"",b,":: ",a+b)
+print("Subtraction of ",a,"",b,":: ",a-b)
+print("Multiplication of ",a,"",b,":: ",a*b)
+print("Division of ",a,"",b,": ",a/b)
+print("Mod of ",a,"",b,": ",a%b)
+print("Floor Division of ",a,"",b,": ",a//b)
+print("Exponentation of ",a,"",b,": ",a**b)
+
+# assignment operator
+a+=3
+print("+=",a)
+a=10
+a-=5
+print("-=",a)
+a=10
+a*=2
+print("*=",a)
+a=10
+a/=1
+print("/=",a)
+a=10
+a//=1
+print("//=",a)
+a=10
+a%=1
+print("%=",a)
+a=10
+a**=1
+print("**=",a)
+
+# comparision operator
+a=20
+print("==",a>=b)
+print("!=",a<=b)
+print(">=",a==b)
+print("<=",a!=b)
+print("<",a<b)
+print(">",a>b)
+
+# logical operator
+a=True
+b=False
+print("and",a and b)
+print("or",a or b)
+print("not",not b)
+
+# bitwise operator
+print("bitwise &",a&b)
+print("bitwise |",a|b)
+print("bitwise ^",a^b)
+print("bitwise >>",a>>b)
+print("bitwise <<",a<<b)
+
+# identity operator
+print("is",a is b)
+print("is not",a is not b)
+
+# membership operator
+a="sangram"
+print('s' in a)
+print("a"  in a)
+#Addition of  10  5 ::  15        
+#Subtraction of  10  5 ::  5      
+#Multiplication of  10  5 ::  50  
+#Division of  10  5 :  2.0        
+#Mod of  10  5 :  0
+#Floor Division of  10  5 :  2    
+#Exponentation of  10  5 :  100000
+#+= 13
+#-= 5
+#*= 20
+#/= 10.0
+#//= 10
+#%= 0
+#**= 10
+#== True
+#!= False
+#>= False
+#<= True
+#< False
+#> True
+#and False
+#or True
+#not True
+#bitwise & False
+#bitwise | True
+#bitwise ^ True
+#bitwise >> 1
+#bitwise << 1
+#is False
+#is not True
+#True
+#True
